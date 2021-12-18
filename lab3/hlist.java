af85b5438f02ec36505686c1ccc94d5771e20d08
CyberSeagull <62190177+CyberSeagull@users.noreply.github.com>
2021-12-18 06:00:45 +0200

    Update hlist.java

--- lab3/hlist.java
+++ lab3/hlist.java
@@ -40,14 +40,40 @@ public class hlist {
         }
         return null;
     }
+    public hobject getmindistance(hobject o) {
+    	double d=999;
+    	double d2=0;
+    	hobject res=null;
+    	for(hobject o2:m_objects) 
+            if (o2!=o) {
+            	d2= PVector.sub(o.position, o2.position).mag();
+//           	 System.out.println("d= "+d+"  d2= "+d2);
+            	if (d>d2) {
+            		d=d2; res=o2;//index = m_objects.indexOf(o2);
+            	}
+            }
+    	
+    	//carsInRoads.contains(car)
+    	  if ((d<o.getRadius()) && (o instanceof wolf) && (!(res instanceof wolf)) )
+    	  {
+    		  res.delobj=true;
+    		  m_objects.remove(res);
+    		  
+            System.out.println("I'm wolf. I eat.");
+    	  }
+    	// System.out.println("o.senseradius= "+o.senseradius+"  d= "+d);
+    	if (d<o.senseradius)
+          return res;
+    	else return null;
+    }
 
     public void update() {
         double delta_t = 1.0 / m_frames_per_second;        
         for(hobject go:m_objects) { go.update(this, delta_t);     
 	 //   System.out.println("------hun_x= "+go.getX()+"  hun_y= "+go.getY()+"  hun_y= "+go.m_x);
         }
-
-        
+	for(int i=m_objects.size()-1; i>=0; i--)
+        if (m_objects.get(i).delobj) m_objects.remove(m_objects.get(i));
     }
         
     public void draw(Graphics2D g) {
