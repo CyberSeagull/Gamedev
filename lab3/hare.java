3d4a29ec834f967dcaea3337d5318d751155d904
CyberSeagull <62190177+CyberSeagull@users.noreply.github.com>
2021-12-19 16:26:10 +0200

    Update hare.java

--- lab3/hare.java
+++ lab3/hare.java
@@ -4,20 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class hare extends hobject {
-  	PVector velocity;// = new PVector();
-  	PVector acceleration;// = new PVector();
-  	Color m_color;
-  	double maxforce;    // Maximum steering force
-  	double maxspeed;    // Maximum speed  	
-  	double r;
-    double d;
-    double dcircle;
-    double anglestep=15;
-    extentcollision m_collision_box;
-    double circledistance =1;
-    double circleradius=1;
-    int anglechangestep=15;
-    int angle =0;   
+  	  
     
     public hare(double x, double y, double r, Color mycolor, double alpha) throws Exception {
         m_x = x;
@@ -27,94 +14,28 @@ public class hare extends hobject {
         omaxspeed=4;
         m_color=mycolor;
         acceleration = new PVector(0,0);
-        velocity = new PVector(3,-2);
+        velocity = new PVector(0,0);
        // velocity.mult(5);;
         position = new PVector(x,y);        
 //        r = 6;
         maxspeed = 4;
-        maxforce = 0.1;      
+        maxforce = 0.14;      
+        circledistance =1;
+        circleradius=1;
+        anglechangestep=15;
+        angle =0;
         d=75;
         
        // m_collision_box = new extentcollision(m_x, m_y, m_r);
     }
 
-    void applyForce(PVector force) {
-      acceleration.add(force);
-    }
-    double map(double val, double start1, double stop1, double start2, double stop2) {
-    	if (Math.abs(stop1-start1)>0.0001)
-    	return val*(stop2-start2)/(stop1-start1);
-    	else return val;
-    }
-    
-    void arrive(PVector target) {
-        PVector desired = PVector.sub(target, position);     
-        double d = desired.mag();
-     
-        if (d < 100) {   // Scale with arbitrary damping within 100 pixels
-          double m = map(d,0,100,0,maxspeed);
-          desired.setmag(m);
-        } else 
-        	 desired.setmag(maxspeed);
-    //The usual steering = desired - velocity
-        PVector steer = PVector.sub(desired,velocity);
-        steer.limit(maxforce);
-        applyForce(steer);
-      }
-
-    void seek(PVector target) {
-        PVector desired = PVector.sub(target,position);  
-                // Scale to maximum speed
-        desired.setmag(maxspeed);
-        // Steering = Desired minus velocity
-        PVector steer = PVector.sub(desired,velocity);
-        steer.limit(maxforce);  // Limit to maximum steering force
         
-        applyForce(steer);
-      }
-            
-    boolean boundaries(double width, double height, double d) {
-    	PVector desired = null;
-    	
-        if (position.x < d) 
-          desired = new PVector(maxspeed, velocity.y);
-        else 
-        	if (position.x > width -d) 
-        		desired = new PVector(-maxspeed, velocity.y);
-        if (position.y < d) 
-          desired = new PVector(velocity.x, maxspeed);
-        else if (position.y > height-d) 
-          desired = new PVector(velocity.x, -maxspeed);
-        if (desired != null) {
-        	desired.normalize();
-            desired.mult(maxspeed);
-            PVector steer = PVector.sub(desired, velocity);
-            steer.limit(maxforce);
-            applyForce(steer);
-          return true;
-        } 
-       else return false;
-      }
-    public void getrandomtarget(PVector futurepos) { //Wandering
-      double rnd= Math.random();
-  	  if (rnd<0.5) angle+=anglechangestep;
-  	  else if (rnd<1) angle-=anglechangestep;
-  	  double angleradr = Math.toRadians(angle);
-
-  	  futurepos.x=velocity.x;
-      futurepos.y=velocity.y;
-      futurepos.setmag(circledistance);
-      futurepos.add(position);  	  
-      PVector vector=new PVector( Math.cos(angleradr), Math.sin(angleradr));
-      vector.setmag(circleradius);
-      futurepos.add(vector);
-    
     public void update(hlist game, double delta_t) {
     	hobject hun =  game.getmindistance(this);//game.m_objects.get(0);
     	PVector target = new PVector(0, 0);
     	//PVector target = new PVector(22, 22);
 //    	arrive(target);
-        	if (!boundaries(game.getWidth(), game.getHeight(), d+m_r/2))
+        	if (!boundaries(game.getWidth(), game.getHeight(), d+m_r/2, game.m_dx, game.m_dy ))
     	{ 	
         		if (hun == null)
         		{ 
@@ -152,8 +73,6 @@ public class hare extends hobject {
         gg.dispose();        
     }
     
-    public extentcollision getCollisionBox() {
-        return m_collision_box;
-    }
+    
     
 }
