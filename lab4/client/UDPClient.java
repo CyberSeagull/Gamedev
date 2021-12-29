1229f9d360ca06416704ca7589117af1575ff99e
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2021-12-29 19:15:21 +0200

    Update UDPClient.java

--- lab4/client/UDPClient.java
+++ lab4/client/UDPClient.java
@@ -11,44 +11,7 @@ public class UDPClient{
   public final static int SERVICE_PORT = 277;
   
   public static void main(String[] args) throws IOException{
-    try{
-      
-      DatagramSocket clientSocket = new DatagramSocket();
-      
-      
-      InetAddress IPAddress = InetAddress.getByName("172.20.12.25");
-      
-      
-      byte[] sendingDataBuffer = new byte[1024];
-      byte[] receivingDataBuffer = new byte[1024];
-      
-      
-      String sentence = "Hello from UDP client";
-      sendingDataBuffer = sentence.getBytes();
-      
-      DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer,sendingDataBuffer.length,IPAddress, SERVICE_PORT);
-      
-      
-      clientSocket.send(sendingPacket);
-      
-      
-      DatagramPacket receivingPacket = new DatagramPacket(receivingDataBuffer,receivingDataBuffer.length);
-      clientSocket.receive(receivingPacket);
-      
-      InetAddress senderAddress = receivingPacket.getAddress();
-      int senderPort = receivingPacket.getPort();
-      System.out.println("senderPort: "+senderPort+"  IP: "+senderAddress.toString());
-      
-      
-      
-      String receivedData = new String(receivingPacket.getData());
-      System.out.println("Sent from the server: "+receivedData);
-      
-      
-      clientSocket.close();
-    }
-    catch(SocketException e) {
-      e.printStackTrace();
-    }
+    glist game = new glist(900, 600, 25);
+		  gwindowUDPclient.newWindow(game);
   }
 }
