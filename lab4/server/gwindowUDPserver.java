61175e3b899052aef05501f977fa1fb05d6b0a30
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2021-12-29 02:55:33 +0200

    Update gwindowUDPserver.java

--- lab4/server/gwindowUDPserver.java
+++ lab4/server/gwindowUDPserver.java
@@ -1,10 +1,13 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -12,91 +15,126 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
 
 
-public class gwindowUDPserver extends JFrame {
+public class gwindowUDPserver extends JComponent {
 	private static final long serialVersionUID = 1L;
 	glist m_glist;
-	JRadioButton rb1,rb2;    
-	JButton b;    
-	JSpinner spinner;
-	JLabel lp1,lp2, lp1w, lp2w;
-	JLabel lpmove, lresult; //2021
-    boolean startgame=false;
     
-    public  gwindowUDPserver() {
-        setBounds(500, 500, 500, 125);
-    	setTitle("LAB-4   UDP SERVER.");
-        int xl=20;
-        int yt=3;
-        JLabel lwolf = new JLabel("COUNT CLIENT: ");   
+    private gwindowUDPserver(glist lclients) {        
+        m_glist = lclients;
+        setPreferredSize(new Dimension((int)m_glist.getWidth(),(int)m_glist.getHeight()));
+    }
+    
+    public static void newWindow(glist clist) {
+    	
+        JFrame frame = new JFrame("LAB-4   UDP SERVER.");
+        gwindowUDPserver c = new gwindowUDPserver(clist);
+        JLabel lclients = new JLabel("COUNT CLIENT: ");   
+        lclients.setBounds(35,3,90,22);
+        frame.add(lclients); 
 
+        JLabel lresult = new JLabel("0777 ");   
+        lresult.setBounds(200,3,75,22);
+        frame.add(lresult); 
+        
         JButton b=new JButton("START SERVER");
-        b.setBounds(550,yt,110,25);    
-        add(b); 
+        b.setBounds(350,3,150,25);    
+        frame.add(b); 
 
-        JLabel lresult = new JLabel("ALL: ");   
-        lresult.setBounds(400, 333,75,22);
-        add(lresult); 
-        
-//        setResizable(false);
-        
-        setLayout(null);    
-        setVisible(true);    
-        
-        
-  //      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  //      setVisible(true);
-  //      repaint();
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().add(c, BorderLayout.CENTER);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.repaint();
 
+        stopstartgame(clist,frame,lresult);
         b.addActionListener(new ActionListener() { //button click
     		
     	    @Override
     	    public void actionPerformed(ActionEvent e) {
-   // 	      startgame=true;
-    	      if (m_glist.stopstart) b.setText("START GAME" );
-    	      else                 b.setText("STOP  GAME" );
-    	      m_glist.stopstart=!m_glist.stopstart;
-              /*
-    	      try {
-            	  hlist.setclearobject(  (Integer)sphare.getValue(), (Integer)spwolf.getValue(), (Integer)spdoe.getValue() );
+    	      if (clist.stopstart) b.setText("START SERVER" );
+    	      else                 b.setText("STOP  SERVER" );
+              clist.stopstart=!clist.stopstart;
+              System.out.println(clist.stopstart+"  ...");
+              
+/*
+              try {
+            	  clist.setclearobject(  );
               } catch (Exception e1) {
 				e1.printStackTrace();
-			}*/
-              
+			}
+              */
               //getValue()
       //          Thread.sleep(12);
              // stopstartgame(hlist,frame);
         //        Thread.sleep(12);
     	    }
     	});	
-        stopstartgame(m_glist, this, lresult);
 
-        
     }
-    
-	   
 
-
+    	
    static void stopstartgame(glist m_glist, JFrame frame, JLabel lresult) {
-    //startgame=true;
     long next_update = System.currentTimeMillis();
     int interval = 1;
     while(true) {
         try {
             long current_time = System.currentTimeMillis();
+
+      //      System.out.println(m_glist.stopstart+"  Waiting for a client to connect...");
+    
             if (!m_glist.stopstart) continue;
-            if (current_time>=next_update) 
-//                synchronized (hlist.class) 
+    //        if (current_time>=next_update) 
+//                synchronized (glist.class) 
             {
             	//m_glist.update();
-            	int counto=m_glist.m_clients.size();
-            	lresult.setText("ALL: "+m_glist.m_clients.size());
+            	lresult.setText(" "+m_glist.m_clients.size());
             	if (m_glist.m_clients.size()==1)
                 frame.repaint();
                 next_update += interval;
                 if (current_time>=next_update + interval*10) next_update = current_time + interval;
+                
+                System.out.println("Waiting for a client to connect...");
+               System.out.println(m_glist.stopstart+"  Waiting for a client to connect...");
+                
+                
+          	  try{
+              	DatagramSocket serverSocket = new DatagramSocket(278);
+              	serverSocket.setSoTimeout(10000);
+                byte[] receivingDataBuffer = new byte[1024];
+                byte[] sendingDataBuffer = new byte[1024];
+                DatagramPacket inputPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
+                System.out.println("Waiting for a client to connect...");
+              	  try {    	  
+                serverSocket.receive(inputPacket);
+                String receivedData = new String(inputPacket.getData());
+                System.out.println("Sent from the client: "+receivedData);
+                sendingDataBuffer = receivedData.toUpperCase().getBytes();
+                InetAddress senderAddress = inputPacket.getAddress();
+                int senderPort = inputPacket.getPort();
+                System.out.println("senderPort: "+senderPort+"  IP: "+senderAddress.toString());
+                DatagramPacket outputPacket = new DatagramPacket(
+                  sendingDataBuffer, sendingDataBuffer.length,
+                  senderAddress,senderPort
+                );
+                serverSocket.send(outputPacket);
+                    } catch (Exception ex) {
+                        System.out.println("Socket error: " + ex.getMessage());
+                    } 
+              	  
+                serverSocket.close();
+              }
+              catch (SocketException e){
+                e.printStackTrace();
+              }                
+                
+                
+                
+                
+                
             }
             Thread.sleep(1);
         }catch(Exception e) {
@@ -104,18 +142,5 @@ public class gwindowUDPserver extends JFrame {
         }
     }
     }   
-    
-    
-    public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D)g;
-    //    g2d.setColor( new Color(8,8,8));
-    //    g2d.fillRect(0, 0, (int)(m_hlist.getWidth()), (int)(m_hlist.getHeight()));
-    //    g2d.setColor( new Color(8,127,8));
-    	//System.out.println("!! lmin= "+999+" C.x=  "+getWidth()+" C.y=  "+ getHeight()  ); 	        
-//        g2d.fillRect(m_hlist.getdx(), m_hlist.getdy(), m_hlist.getWidth()-m_hlist.getdx(), m_hlist.getHeight()-m_hlist.getdy());
-        
-        //g2d.fillRect(10,30, (int)(m_hlist.getWidth())-10, (int)(m_hlist.getHeight())-30);
-        //g2d.fillRect(10, 30, (m_hlist.getWidth()-m_hlist.getdx()), (m_hlist.getHeight()-m_hlist.getdy()));
-  //      m_hlist.draw(g2d);
-    }    
+
 }
