0c3306d93db416f1f6dd5f58f6656adc43984ece
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2021-12-29 02:51:39 +0200

    Update gclient.java

--- lab4/server/gclient.java
+++ lab4/server/gclient.java
@@ -1,4 +1,20 @@
+import java.net.InetAddress;
+
 
 public class gclient {
+  InetAddress senderAddress;
+    boolean rflag1,rflag2;
+    boolean sflag1,sflag2;
+    public byte[] rBuff1 = new byte[1024]; //receivingDataBuffer
+    public byte[] sBuff1 = new byte[1024]; //sendingDataBuffer
+    public byte[] rBuff2 = new byte[1024]; //receivingDataBuffer
+    public byte[] sBuff2 = new byte[1024]; //sendingDataBuffer
+
+    public gclient(InetAddress senderAddress) throws Exception {
+        this.senderAddress = senderAddress;
+    }	
+    public InetAddress getIP() {
+        return senderAddress;
+    }
 
 }
