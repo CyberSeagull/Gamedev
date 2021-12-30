2476aff99e00661ab0c6a9cd0127a2cf5e0c1b32
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2021-12-30 23:14:21 +0200

    Update food.java

--- lab4/client/food.java
+++ lab4/client/food.java
@@ -1,5 +1,18 @@
 package pwclient;
 
+import java.awt.Graphics2D;
+
 public class food  extends gobject {
 
+	@Override
+	public void update(glist game, double delta_t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
 }
