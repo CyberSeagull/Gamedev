22202edc71a927d7e228bc7b5014d7c20b3c37dd
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2021-12-30 23:15:10 +0200

    Update gamer.java

--- lab4/client/gamer.java
+++ lab4/client/gamer.java
@@ -1,5 +1,17 @@
 package pwclient;
+import java.awt.Graphics2D;
 
 public class gamer extends gobject {
+  @Override
+	public void update(glist game, double delta_t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
