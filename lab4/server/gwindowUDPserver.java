c70fbfdef6f56044407a486585aaf0de8fd05f86
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2021-12-30 23:11:02 +0200

    Update gwindowUDPserver.java

--- lab4/server/gwindowUDPserver.java
+++ lab4/server/gwindowUDPserver.java
@@ -26,12 +26,11 @@ public class gwindowUDPserver extends JComponent {
     }
     
     public static void newWindow(glist clist) {
-    int yp=15;
+    	int yp=15;
     	int xp=5;
         JFrame frame = new JFrame("LAB-4   UDP SERVER.");
         gwindowUDPserver c = new gwindowUDPserver(clist);
 
-    	
         JLabel lroomw = new JLabel("Game room width: ");   
         lroomw.setBounds(xp,yp,107,22);
         frame.add(lroomw); 
@@ -54,7 +53,7 @@ public class gwindowUDPserver extends JComponent {
         frame.add(lresult); 
         
         yp=50;
-        JLabel lport = new JLabel("¹ PORT: ");   
+        JLabel lport = new JLabel("� PORT: ");   
         lport.setBounds(xp+180,yp,70,22);
         frame.add(lport); 
         JSpinner spport = new JSpinner(new SpinnerNumberModel(278, 1, 65535, 1));   
@@ -72,101 +71,50 @@ public class gwindowUDPserver extends JComponent {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.repaint();
+        try{
+        	clist.setPort((Integer) spport.getValue());
+    		clist.servert = new dtgserver( (Integer) spport.getValue());
+            UDPreceivedata ct = new UDPreceivedata(clist.servert, clist);
+            ct.start();
+              //System.out.println("Start UDPreceivedata from the client: "+ct.isAlive());
+        } catch (Exception ex) {
+              System.out.println("Socket error: " + ex.getMessage());
+        } 
 
         b.addActionListener(new ActionListener() { //button click
-    		
     	    @Override
     	    public void actionPerformed(ActionEvent e) {
     	      if (clist.stopstart) b.setText("START SERVER" );
     	      else                 b.setText("STOP  SERVER" );
               clist.stopstart=!clist.stopstart;
               System.out.println(clist.stopstart+"  ...");
-              
     	    }
-    	});
-    		stopstartgame(clist,frame,lresult);
-
+    	});	
+        stopstartgame(clist,frame,lresult);
     }
 
     	
-   static void stopstartgame(glist m_glist, JFrame frame, JLabel lresult) {
-    long next_update = System.currentTimeMillis();
-    int interval = 1;
-    while(true) {
-        try {
-            long current_time = System.currentTimeMillis();
-
-          //  System.out.println(m_glist.stopstart+"  ssssssssssssssst...");
-            Thread.sleep(1);
-            
-            if (!m_glist.stopstart) {
-        //    	System.out.println(m_glist.stopstart+"  CONTINUE...");
-            continue;
-            }
-          System.out.println(m_glist.stopstart+"  1  Waiting for a client to connect...");
-
-            {
-            	//m_glist.update();
-            	lresult.setText(" "+m_glist.m_clients.size());
-            	//  if (m_glist.m_clients.size()==1)
-            //  frame.repaint();
-            //  next_update += interval;
-            //  if (current_time>=next_update + interval*10) next_update = current_time + interval;
-                
-                System.out.println("Waiting for a client to connect...");
-               System.out.println(m_glist.stopstart+"  Waiting for a client to connect...");
-                
-                
-          	  try{
-/*
-              	DatagramSocket serverSocket = new DatagramSocket(278);
-              	serverSocket.setSoTimeout(10000);
-                byte[] receivingDataBuffer = new byte[1024];
-                byte[] sendingDataBuffer = new byte[1024];
-                DatagramPacket inputPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
-*/
-
-          		dtgserver serverSocket = new dtgserver(278);
-                serverSocket.setSoTimeout(10000);
-                byte[] receivingDataBuffer = new byte[1024];
-                byte[] sendingDataBuffer = new byte[1024];
-                DatagramPacket inputPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
-
-
-                System.out.println("Waiting for a client to connect...");
-              	  try {    	  
-                serverSocket.receive(inputPacket);
-                String receivedData = new String(inputPacket.getData());
-                System.out.println("Sent from the client: "+receivedData);
-                sendingDataBuffer = receivedData.toUpperCase().getBytes();
-                InetAddress senderAddress = inputPacket.getAddress();
-                int senderPort = inputPacket.getPort();
-                System.out.println("senderPort: "+senderPort+"  IP: "+senderAddress.toString());
-                DatagramPacket outputPacket = new DatagramPacket(
-                  sendingDataBuffer, sendingDataBuffer.length,
-                  senderAddress,senderPort
-                );
-                serverSocket.send(outputPacket);
-                    } catch (Exception ex) {
-                        System.out.println("Socket error: " + ex.getMessage());
-                    } 
-              	  
-                serverSocket.close();
-              }
-              catch (SocketException e){
-                e.printStackTrace();
-              }                
-                
-                
-                
-                
-                
-            }
-            Thread.sleep(1);
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-    }   
+   static void stopstartgame(glist hlistu, JFrame frame, JLabel lresult) {
+		    //startgame=true;
+	        long next_update = System.currentTimeMillis();
+	        int interval = 1000 / hlistu.getFPS();
+	        while(true) {
+	            try {
+	                long current_time = System.currentTimeMillis();
+	                Thread.sleep(12);
+	                if (!hlistu.stopstart) continue;
+	                if (current_time>=next_update) 
+	                {
+	                	lresult.setText("ALL: "+hlistu.m_clients.size());
+	                	hlistu.setallposition();
+	                    next_update += interval;
+	                    if (current_time>=next_update + interval*10) next_update = current_time + interval;
+	                }
+	                Thread.sleep(1);
+	            }catch(Exception e) {
+	                e.printStackTrace();
+	            }
+	        }
+		    }   
 
 }
