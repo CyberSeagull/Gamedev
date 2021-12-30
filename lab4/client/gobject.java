292d14f95721c65b442faf7ef2e3df64bdb0a455
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2021-12-30 23:17:39 +0200

    Update gobject.java

--- lab4/client/gobject.java
+++ lab4/client/gobject.java
@@ -1,10 +1,19 @@
 package pwclient;
+import java.awt.Graphics2D;
 
 public class gobject {
+    PVector position;
   	double m_x, m_y, m_r;
     double senseradius=0;
     double ominspeed=1;
     double omaxspeed=3;
     boolean delobj=false;
+  
+  public abstract void update(glist game, double delta_t);
+    public abstract void draw(Graphics2D g);
+
+    public double getRadius() {
+        return m_r;
+    }
 
 }
