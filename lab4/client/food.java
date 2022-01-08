dbc0b45367c45c996481a16a7af4bb3780ac6e95
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2022-01-09 00:38:12 +0200

    Update food.java
    visualized food

--- lab4/client/food.java
+++ lab4/client/food.java
@@ -1,18 +1,28 @@
 package pwclient;
-
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class food  extends gobject {
 
-	@Override
-	public void update(glist game, double delta_t) {
-		// TODO Auto-generated method stub
-		
-	}
+	public food(PVector pos, double r)  {
+        position = pos;//new PVector(x,y);
+        poscl    = new PVector(pos.x,pos.y);
+        m_r = r;
+        m_color = Color.yellow;
+        Visibleobject=false;
+    }
 
 	@Override
-	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
+	public void update(glist game, double delta_t) {
 		
+		Visibleobject = (GetLengthtotheWalls(game.getWidth(), game.getHeight(), position, m_r, game.m_dx, game.m_dy )>0);
+	}
+    public void draw(Graphics2D g) {
+    	if (Visibleobject) {
+        Graphics2D gg = (Graphics2D) g.create();
+        gg.setColor(m_color);
+        gg.fillOval((int)(position.x-m_r/2), (int)(position.y-m_r/2), (int)m_r, (int)m_r);
+        gg.dispose();     
+    	}
 	}
 }
