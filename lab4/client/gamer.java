baafe00f0d632893c2cb6674b119989d27e7ffe1
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2022-01-09 00:39:56 +0200

    Update gamer.java
    visualized other players (not the main one)

--- lab4/client/gamer.java
+++ lab4/client/gamer.java
@@ -2,15 +2,24 @@ package pwclient;
 import java.awt.Graphics2D;
 
 public class gamer extends gobject {
-  @Override
+   public gamer(PVector pos, double r, Color mycolor)  {
+        position = pos;//new PVector(x,y);        
+        poscl    = new PVector(pos.x,pos.y);
+        m_color=mycolor;
+        m_r = r;
+    }
+	@Override
 	public void update(glist game, double delta_t) {
-		// TODO Auto-generated method stub
 		
+		Visibleobject = (GetLengthtotheWalls(game.getWidth(), game.getHeight(), position, m_r, game.m_dx, game.m_dy )>0);		
 	}
-
 	@Override
 	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
+		Graphics2D gg = (Graphics2D) g.create();
+        	gg.setColor(m_color);
+        	gg.fillOval((int)(position.x-m_r/2), (int)(position.y-m_r/2), (int)m_r, (int)m_r);
+//    		System.out.println("!! m_x= "+m_x+"  m_y= "+m_y+" m_r= "+m_r+"  ");
+        	gg.dispose(); 
 		
 	}
 
